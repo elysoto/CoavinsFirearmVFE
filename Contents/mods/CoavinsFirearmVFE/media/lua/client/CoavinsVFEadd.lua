5a13c00607acf5ce8826f0f017938454df075217
elysoto <elysoto@users.noreply.github.com>
2024-12-08 17:36:39 -0500

    Update VFE

--- Contents/mods/CoavinsFirearmVFE/media/lua/client/CoavinsVFEadd.lua
+++ Contents/mods/CoavinsFirearmVFE/media/lua/client/CoavinsVFEadd.lua
@@ -1,33 +1,40 @@
 
-CoavinsFirearms.Include('Base.Shotgun',                             'GenericShotgun') -- M500 Pump Shotgun
-CoavinsFirearms.Include('Base.ShotgunSawnoff',                      'GenericShotgun') -- M500 Pump Shotgun
-CoavinsFirearms.Include('Base.ShotgunSawnoffNoStock',               'GenericShotgun') -- M500 Pump Shotgun
-CoavinsFirearms.Include('Base.Spas12',                              'GenericShotgun') -- SP-12 Pump Shotgun
-CoavinsFirearms.Include('Base.Spas12Folded',                        'GenericShotgun') -- SP-12 Pump Shotgun
-CoavinsFirearms.Include('Base.Shotgun2',                            'GenericShotgun') -- M590 Pump Shotgun
-CoavinsFirearms.Include('Base.Shotgun2Bayonet',                     'GenericShotgun') -- M590 Pump Shotgun
+CoavinsFirearms.Include('Base.Shotgun',                 'GenericShotgun') -- M500 Pump Shotgun
+CoavinsFirearms.Include('Base.ShotgunSawnoff',          'GenericShotgun') -- M500 Pump Shotgun
+CoavinsFirearms.Include('Base.ShotgunSawnoffNoStock',   'GenericShotgun') -- M500 Pump Shotgun
+CoavinsFirearms.Include('Base.Spas12',                  'GenericShotgun') -- SP-12 Pump Shotgun
+CoavinsFirearms.Include('Base.Spas12Folded',            'GenericShotgun') -- SP-12 Pump Shotgun
+CoavinsFirearms.Include('Base.Shotgun2',                'GenericShotgun') -- M590 Pump Shotgun
+CoavinsFirearms.Include('Base.Shotgun2Bayonet',         'GenericShotgun') -- M590 Pump Shotgun
 
-CoavinsFirearms.Include('Base.DoubleBarrelShotgun',                 'Shotgun_20g') -- Double Barrel
-CoavinsFirearms.Include('Base.DoubleBarrelShotgunSawnoff',          'Shotgun_20g')
-CoavinsFirearms.Include('Base.DoubleBarrelShotgunSawnoffNoStock',   'Shotgun_20g')
+CoavinsFirearms.Include('Base.DoubleBarrelShotgun',               'Shotgun_20g') -- Double Barrel
+CoavinsFirearms.Include('Base.DoubleBarrelShotgunSawnoff',        'Shotgun_20g')
+CoavinsFirearms.Include('Base.DoubleBarrelShotgunSawnoffNoStock', 'Shotgun_20g')
 
-CoavinsFirearms.Include('Base.ShotgunSemi',                         'Shotgun_12g') -- R1187 SemiShotgun
-CoavinsFirearms.Include('Base.ShotgunSemi2',                        'Shotgun_12g') -- R1187 SemiShotgun
-CoavinsFirearms.Include('Base.ShotgunSilent',                       'Shotgun_12g') -- Super Rare Coin Toss Gun
+CoavinsFirearms.Include('Base.ShotgunSemi',             'Shotgun_12g') -- R1187 SemiShotgun
+CoavinsFirearms.Include('Base.ShotgunSemi2',            'Shotgun_12g') -- R1187 SemiShotgun
+CoavinsFirearms.Include('Base.ShotgunSilent',           'Shotgun_12g') -- Super Rare Coin Toss Gun
 
-CoavinsFirearms.Include('Base.AK47Unfolded',    'AK47')
-CoavinsFirearms.Include('Base.AK47',            'AK47')
-CoavinsFirearms.Include('Base.AK47Folded',      'AK47')
+CoavinsFirearms.Include('Base.M2400_Shotgun',     'Shotgun_12g')
+CoavinsFirearms.Include('Base.M2400_Rifle',       'Rifle_308BA')
 
-CoavinsFirearms.Include('Base.SKS',                 'SKS')
-CoavinsFirearms.Include('Base.SKSSpiker',           'SKS')
-CoavinsFirearms.Include('Base.SKSSpikerBayonet',    'SKS')
+CoavinsFirearms.Include('Base.AK47Unfolded',    'AK47') -- 762Bullets
+CoavinsFirearms.Include('Base.AK47',            'AK47') -- 762Bullets
+CoavinsFirearms.Include('Base.AK47Folded',      'AK47') -- 762Bullets
+
+CoavinsFirearms.Include('Base.MAK90',               'SKS') -- 762Bullets
+CoavinsFirearms.Include('Base.SKS',                 'SKS') -- 762Bullets
+CoavinsFirearms.Include('Base.SKSSpiker',           'SKS') -- 762Bullets
+CoavinsFirearms.Include('Base.SKSSpikerBayonet',    'SKS') -- 762Bullets
 
 CoavinsFirearms.Include('Base.Mini14Unfolded',  'Rifle_556')
 CoavinsFirearms.Include('Base.Mini14',          'Rifle_556')
 CoavinsFirearms.Include('Base.Mini14Folded',    'Rifle_556')
 CoavinsFirearms.Include('Base.FAL',             'Rifle_556')
 CoavinsFirearms.Include('Base.FALClassic',      'Rifle_556')
+
+CoavinsFirearms.Include('Base.AssaultRifleMasterkeyShotgun', 'GenericShotgun') -- ShotgunShells
+CoavinsFirearms.Include('Base.AssaultRifleMasterkey',      'Rifle_556') -- 556Bullets
 
 CoavinsFirearms.Include('Base.CAR15Folded',     'M16Rifle')
 CoavinsFirearms.Include('Base.CAR15DFolded',    'M16Rifle')
@@ -38,13 +45,17 @@ CoavinsFirearms.Include('Base.AssaultRifle2',   'M16Rifle')
 
 CoavinsFirearms.Include('Base.AssaultRifleM1',      'Rifle_308BA')
 CoavinsFirearms.Include('Base.AssaultRifleBayonet', 'Rifle_308BA')
+CoavinsFirearms.Include('Base.M60MMG',              'Rifle_308BA') -- 308BulletsLinked
+CoavinsFirearms.Include('Base.M60MMG_Bipod',        'Rifle_308BA') -- 308BulletsLinked
 
-CoavinsFirearms.Include('Base.SniperRifle',  'Rifle_308AR') -- M24 Rifle
+CoavinsFirearms.Include('Base.SniperRifle',  'Rifle_308AR') -- 308Bullets - M24 Rifle
 
-CoavinsFirearms.Include('Base.1022',            'BoltActionRifle')
-CoavinsFirearms.Include('Base.HuntingRifle',    'BoltActionRifle')
-CoavinsFirearms.Include('Base.VarmintRifle',    'BoltActionRifle')
-CoavinsFirearms.Include('Base.LeverRifle',      'BoltActionRifle') -- W1866 Carbine
+CoavinsFirearms.Include('Base.CampCarbine',     'BoltActionRifle') -- Bullets45
+CoavinsFirearms.Include('Base.1022',            'BoltActionRifle') -- 22Bullets
+CoavinsFirearms.Include('Base.HuntingRifle',    'BoltActionRifle') -- 308Bullets
+CoavinsFirearms.Include('Base.VarmintRifle',    'BoltActionRifle') -- 223Bullets
+CoavinsFirearms.Include('Base.LeverRifle',      'BoltActionRifle') -- Bullets38 - W1866 Carbine 
+CoavinsFirearms.Include('Base.LeverRifle2',     'BoltActionRifle') -- Bullets44
 
 CoavinsFirearms.Include('Base.MP5',          'Pistol_9mm')
 CoavinsFirearms.Include('Base.MP5SD',        'Pistol_9mm')
@@ -55,20 +66,21 @@ CoavinsFirearms.Include('Base.P229',         'Pistol_9mm')
 CoavinsFirearms.Include('Base.CZ75',         'Pistol_9mm')
 CoavinsFirearms.Include('Base.Pistol',       'Pistol_9mm') -- M9 Pistol
 CoavinsFirearms.Include('Base.Glock',        'Pistol_9mm') -- G17 Pistol
-CoavinsFirearms.Include('Base.Glock18',      'Pistol_9mm') -- G17 Pistol (Duplicate in VFE)
+CoavinsFirearms.Include('Base.Glock18',      'Pistol_9mm') -- G18 Pistol
+CoavinsFirearms.Include('Base.UziUnfolded',  'Pistol_9mm')
+CoavinsFirearms.Include('Base.UziFolded',    'Pistol_9mm')
 
 CoavinsFirearms.Include('Base.Pistol3',        'Pistol_44cal') -- Desert Eagle
 
-CoavinsFirearms.Include('Base.MAC10Unfolded',   'Pistol_45acp')
-CoavinsFirearms.Include('Base.MAC10Folded',     'Pistol_45acp')
-CoavinsFirearms.Include('Base.MK23SOCOM',       'Pistol_45acp')
-CoavinsFirearms.Include('Base.MK2',             'Pistol_45acp')
-CoavinsFirearms.Include('Base.MK2SD',           'Pistol_45acp')
-CoavinsFirearms.Include('Base.Pistol2',         'Pistol_45acp') -- M1911
+CoavinsFirearms.Include('Base.MAC10Unfolded',   'Pistol_45acp') -- Bullets45
+CoavinsFirearms.Include('Base.MAC10Folded',     'Pistol_45acp') -- Bullets45
+CoavinsFirearms.Include('Base.MK23SOCOM',       'Pistol_45acp') -- Bullets45
+CoavinsFirearms.Include('Base.Pistol2',         'Pistol_45acp') -- Bullets45 - M1911
+
+CoavinsFirearms.Include('Base.MK2',             'Pistol_22lr') -- 22lr
+CoavinsFirearms.Include('Base.MK2SD',           'Pistol_22lr') -- 22lr
 
 CoavinsFirearms.Include('Base.Revolver_Short', 'Revolver_38spc') -- M10-5 Revolver
-CoavinsFirearms.Include('Base.Revolver_Long',  'GenericRevolver') -- Magnum 44cal
+CoavinsFirearms.Include('Base.Revolver_Long',  'GenericRevolver') -- Bullets44 - Magnum 44cal
 CoavinsFirearms.Include('Base.Revolver',       'Revolver_45lc') -- M625 Revolver
-
-
 
